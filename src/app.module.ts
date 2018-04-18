6538e77fd039884a9fa4c6b5ed61d6efd319a795
Alioune Kasse <akasse005@gmail.com>
2018-04-18 20:23:46 +0000

    Relation Un à Plusieurs/Plusieurs àun, Relation Plusieurs à Plusieurs  (OneToMany/ManyToOne  ManyToMany)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PhotoModule } from './photo/photo.module';
 
+import { ProjetModule } from './projet/projet.module';
+import { UtilisateurModule } from './utilisateur/utilisateur.module';
+
 @Module({
-  imports: [TypeOrmModule.forRoot(), PhotoModule],
+  imports: [TypeOrmModule.forRoot(), PhotoModule, UtilisateurModule,ProjetModule],
 })
 export class ApplicationModule {}
